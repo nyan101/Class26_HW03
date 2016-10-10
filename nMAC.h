a05bc19fca0b6a5876588c765a611d7a5d5ee9dd
nyan101 <k.minchul95@gmail.com>
2016-10-11 07:27:34 +0900

    = 연산자 오류 해결(리턴타입이 T가 아니라 T&)

--- nMAC.h
+++ nMAC.h
@@ -14,9 +14,16 @@ struct nMAC {
 
     operator const char*() { ether_ntoa_r((const ether_addr*)MAC, MACstr); return MACstr;  }
 
-    void operator = (const nMAC& rhs)       { memcpy(MAC, rhs.MAC, ETHER_ADDR_LEN);	return; }
-    void operator = (const char* rhs)       { ether_aton_r(rhs, (ether_addr*)MAC);  return; }
-    void operator = (const ether_addr& rhs) { memcpy(MAC, rhs.ether_addr_octet, ETHER_ADDR_LEN); return; }
+    nMAC& operator = (const nMAC& rhs)       { memcpy(MAC, rhs.MAC, ETHER_ADDR_LEN);    return *this; }
+    nMAC& operator = (const char* rhs)       { ether_aton_r(rhs, (ether_addr*)MAC);     return *this; }
+    nMAC& operator = (const ether_addr& rhs) { memcpy(MAC, rhs.ether_addr_octet, ETHER_ADDR_LEN); return *this; }
+
+    bool operator == (const char* rhs) {
+        uint8_t tmp[ETHER_ADDR_LEN];
+        ether_aton_r(rhs, (ether_addr*)tmp);
+        return memcmp(tmp, MAC, ETHER_ADDR_LEN)==0;
+    }
+    bool operator == (const ether_addr& rhs) const { return memcmp(MAC, rhs.ether_addr_octet, ETHER_ADDR_LEN)==0; }
 
     bool isBroadcast() const { return memcmp(MAC, "\xff\xff\xff\xff\xff\xff", ETHER_ADDR_LEN)==0; }
 };
