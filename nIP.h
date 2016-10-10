776778c65d21ce87a4f64a140f0cc325846eae12
nyan101 <k.minchul95@gmail.com>
2016-10-10 10:01:03 +0900

    nIP 구조체 설계(gip의 구조를 참고함)

---
+++ nIP.h
@@ -0,0 +1,28 @@
+#include <cstdint>
+#include <cstring>
+#include <arpa/inet.h>
+
+struct nIP {
+    uint32_t IP; // in Network Byte Order
+    char IPstr[INET_ADDRSTRLEN];
+
+    nIP(){}
+    nIP(const nIP& rhs) 	{ IP = rhs.IP; }
+    nIP(const uint32_t rhs) { IP = htonl(rhs); }
+    nIP(const char* rhs) 	{ operator =(rhs); }
+    nIP(const in_addr& rhs) { operator =(rhs); }
+
+    operator uint32_t() const { return ntohl(IP); }
+    explicit operator const char*() { inet_ntop(AF_INET, &IP, IPstr, INET_ADDRSTRLEN); return IPstr; }
+
+    nIP operator = (const nIP& rhs)		{ IP = rhs.IP;	    return *this; }
+    nIP operator = (const uint32_t rhs) { IP = htonl(rhs);	return *this; }
+    nIP operator = (const char* rhs)	{ inet_pton(AF_INET, rhs, &IP); return *this; }
+    nIP operator = (const in_addr& rhs)	{ IP = rhs.s_addr;  return *this; }
+
+    bool operator == (const uint32_t rhs) const { return IP==htonl(rhs); }
+    bool operator == (const in_addr& rhs) const { return IP==rhs.s_addr; }
+    bool operator == (const char* rhs) { return inet_addr(rhs)==IP; } // "invalid string == 255.255.255.255" is true. (use isBroadcast in this case)
+
+    bool isBroadcast() const { return IP == 0xFFFFFFFF; } // 255.255.255.255
+};
